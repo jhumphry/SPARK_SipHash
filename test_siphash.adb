b658634b0934686ffb8f6be829533d31ba9d938a
James Humphry <git@binecho.co.uk>
2015-08-14 22:03:46 +0100

    Test routines over variable-length input
    Tested up to inputs of 2000 in length.

--- test_siphash.adb
+++ test_siphash.adb
@@ -4,12 +4,14 @@
 -- by Jean-Philippe Aumasson and Daniel J. Bernstein
 
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Containers;
 
 with Interfaces, Interfaces.C, Interfaces.C.Strings;
 use Interfaces;
 
 with System.Storage_Elements;
+use System.Storage_Elements;
 
 with SipHash, SipHash.Discrete;
 with SipHash24_c;
@@ -30,9 +32,9 @@ procedure Test_SipHash is
                                  Hash_Type => Ada.Containers.Hash_Type);
 
    -- This matches the test vector setup in the paper.
-   K : constant System.Storage_Elements.Storage_Array :=
+   K : constant Storage_Array :=
      (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
-   M : constant System.Storage_Elements.Storage_Array :=
+   M : constant Storage_Array :=
      (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14);
    Result : Unsigned_64;
 
@@ -91,5 +93,34 @@ begin
    Put("Result received from reference C routine: ");
    Put(C_Result, Base => 16); New_Line;
    C.Strings.Free(Test_C_String);
+   New_Line;
+
+   Put_Line("Testing Ada vs C routine for input lengths from 1 to 2000 bytes.");
+   for I in 1..2000 loop
+      declare
+         M : System.Storage_Elements.Storage_Array(0..Storage_Offset(I-1));
+         C_M : aliased SipHash24_c.U8_Array(0..I-1);
+      begin
+         for J in 0..I-1 loop
+            M(Storage_Offset(J)) := Storage_Element(J mod 256);
+            C_M(J) := Unsigned_8(J mod 256);
+         end loop;
+         Result := Test_SipHash24.SipHash(M);
+         Discard := SipHash24_c.C_SipHash24(c_out => C_Output(0)'Access,
+                                            c_in => C_M(0)'Access,
+                                            inlen => Unsigned_64(I),
+                                            k => C_K(0)'Access);
+         C_Result := SipHash24_c.U8_Array8_to_U64(C_Output);
+         if Result /= C_Result then
+            Put("Difference in result for: "); Put(I); New_Line;
+            Put("Ada code gives: "); Put(Result, Base => 16);
+            Put(" C code gives: "); Put(C_Result, Base => 16);
+            New_Line;
+         end if;
+      end;
+      if I mod 200 = 0 then
+         Put("Tested lengths up to: "); Put(I); New_Line;
+      end if;
+   end loop;
 
 end Test_SipHash;
