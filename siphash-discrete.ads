88bdab5a658ab3494a731f694397fc8031c33550
James Humphry <git@binecho.co.uk>
2015-08-16 13:38:42 +0100

    Move SPARK_Mode on SipHash.Discrete
    This may be necessary if it is to be instantiated below library-level.

--- siphash-discrete.ads
+++ siphash-discrete.ads
@@ -4,11 +4,13 @@
 -- This generic function will calculate SipHash over arrays of a discrete type
 -- indexed by an integer type, such as the standard String type. The range of
 -- values of the discrete type needs to fit in 8 bits.
+
+pragma SPARK_Mode;
+
 generic
    type T is (<>);
    type T_Index is range <>;
    type T_Array is array (T_Index range <>) of T;
    type Hash_Type is mod <>;
 function SipHash.Discrete (m : T_Array) return Hash_Type
-  with SPARK_Mode,
-  Global => (Input => Initial_Hash_State);
+  with Global => (Input => Initial_Hash_State);
